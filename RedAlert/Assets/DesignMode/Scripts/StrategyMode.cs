0c722e0ccc8fe2e6314b707798403c8c80151a15
GitOfDirector <1191549194@qq.com>
2018-10-17 18:23:06 +0800

    so annoying, no one send me a offer, even an interview invitatione

---
+++ RedAlert/Assets/DesignMode/Scripts/StrategyMode.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrategyMode : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        StrategyContext context = new StrategyContext();
+        context.stategy = new ConcreteStrategyA();
+        context.Cal();
+    }
+
+
+}
+
+public class StrategyContext 
+{
+    public IStrategy stategy;
+
+    public void Cal() 
+    {
+        stategy.Cal();
+    }
+
+}
+
+public interface IStrategy 
+{
+    void Cal();
+}
+
+public class ConcreteStrategyA : IStrategy 
+{
+
+    public void Cal()
+    {
+        Debug.Log("使用A策略");
+    }
+}
+
+public class ConcreteStrategyB : IStrategy
+{
+
+    public void Cal()
+    {
+        Debug.Log("使用B策略");
+    }
+}
+
+
